b167397059e8cb3e7895bc2ac1e70337a3f3626d
abehidek <hidek.abe@outlook.com>
2022-05-10 20:30:33 -0300

    Redesign entire page

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,57 @@
-function scrollToElement(id) {
-  document.querySelector(id).scrollIntoView({
-  behavior: 'smooth'
-  })
+class MobileNav {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+    // this.scrollToElement = this.scrollToElement.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+    })
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.animateLinks();
+    this.mobileMenu.classList.toggle(this.activeClass);
+  }
+  
+  showElement(evt) {
+    const className = evt.currentTarget.innerText.toLowerCase()
+    console.log(className)
+    // const page = document.querySelector(".about");
+    // page.classList.remove("active");
+    document.querySelector("."+className).classList.add("active")
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    this.navLinks.forEach((link) => {
+      link.addEventListener("click", this.handleClick);
+      link.addEventListener("click", this.showElement);
+    });
+    //this.navHomeLink.addEventListener("click", this.scrollToHome);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
 }
+
+const mobileNav = new MobileNav(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+
+mobileNav.init();
