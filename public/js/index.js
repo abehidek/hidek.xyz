c7011ba773150dcba0e6ba01affb02f1a98b4301
abehidek <hidek.abe@outlook.com>
2022-05-12 23:46:21 -0300

    Fix anchors bug

--- public/js/index.js
+++ public/js/index.js
@@ -4,28 +4,32 @@ let mobmenu = document.querySelector(".mobile-menu")
 window.addEventListener("DOMContentLoaded", () => {
 	console.log("INIT");
 	const links = document.querySelectorAll('.page-link');
+  const anchors = document.querySelectorAll('.page-anchor');
 
   links.forEach((link) => {
     link.addEventListener("click", e => {
+      let target = e.target.href;
       e.preventDefault();
-      console.log(e.target.href)
-      if (e.target.href.includes("#")) {
-        const urlArray = e.target.href.split('/')
-        const url = urlArray[urlArray.length-1]
-        document.querySelector(url).scrollIntoView({
-          behavior: 'smooth'
-        })
-      }
-      else {
-        let target = e.target.href;
-        intro.classList.remove("end");
-        mobmenu.classList.remove("show");
-        setTimeout(() => {
-          window.location.href = target;
-        }, 500)
-      }
+      intro.classList.remove("end");
+      mobmenu.classList.remove("show");
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500)
     })
   })
+
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener("click", e => {
+      e.preventDefault();
+      const urlArray = e.target.href.split('/')
+      const url = urlArray[urlArray.length-1]
+      document.querySelector(url).scrollIntoView({
+        behavior: 'smooth'
+      })
+    })
+  })
+
 	setTimeout(() => {
     intro.classList.add("end");
     mobmenu.classList.add("show")
@@ -90,116 +94,3 @@ const mobileNav = new MobileNav(
 );
 
 mobileNav.init();
-
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#000"
-    },
-    "shape": {
-      "type": "triangle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#000",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "window",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
-
