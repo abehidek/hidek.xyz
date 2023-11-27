8a0371a485095700973583c4da4e63e8366fff7c
abehidek <hidek.abe@outlook.com>
2023-11-27 13:23:20 -0300

    ref: index.heex -> render/1 callback

--- lib/hidek_xyz_web/live/index_live.ex
+++ lib/hidek_xyz_web/live/index_live.ex
@@ -5,4 +5,13 @@ defmodule HidekXyzWeb.IndexLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1>Welcome to new.hidek.xyz</h1>
+    </div>
+    """
+  end
 end
